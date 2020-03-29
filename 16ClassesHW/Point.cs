841bd9dcdf9a6a8dd735019b0dd41363adfa4b67
Grek <grrekov@yandex.ru>
2020-03-29 23:46:30 +0300

    Добавьте файлы проекта.

---
+++ 16ClassesHW/Point.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _16ClassesHW
+{
+    class Point
+    {
+        private int x;
+        private int y;
+        private int z;
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+            set { }
+        }
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+            set { }
+        }
+        public int Z
+        {
+            get
+            {
+                return z;
+            }
+            set { }
+        }
+
+        public Point(){}
+        public Point(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Point operator +(Point p1, Point p2)
+        {
+            return new Point(p1.X + p2.X, p1.Y + p2.Y, p1.Z + p2.Z);
+        }
+    }
+}
---
+++ 16ClassesHW/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _16ClassesHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point one = new Point(1, 2, 3);
+            Point two = new Point(1, 4, 9);
+            Point three = one + two;
+            Console.WriteLine(three.X + " " + three.Y + " " + three.Z);
+            Console.ReadLine();
+        }
+    }
+}
